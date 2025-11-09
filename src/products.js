93f8271b6052afda2790e921586a54c83ffe0939
Luka Popovic <lukapopovic0111@gmail.com>
2025-11-09 15:20:16 +0100

    Add cart button and empty cart styles

--- src/products.js
+++ src/products.js
@@ -2,25 +2,25 @@ export const productsArray = [
   {
     name: "Sushi",
     description: "Finest fish and veggies",
-    price: "22.99",
+    price: 22.99,
     id: 1,
   },
   {
     name: "Schnitzel",
     description: "A german specialty!",
-    price: "16.5",
+    price: 16.5,
     id: 2,
   },
   {
     name: "Barbecue Burger",
     description: "American, raw, meaty",
-    price: "12.99",
+    price: 12.99,
     id: 3,
   },
   {
     name: "Green Bowl",
     description: "Healthy...and green...",
-    price: "18.99",
+    price: 18.99,
     id: 4,
   },
 ];
